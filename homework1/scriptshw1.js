fbf0f11788a00fc0e010d8bf7e6e802c02ef6f69
Patsfirstdown <43241638+Patsfirstdown@users.noreply.github.com>
2025-10-26 12:45:32 -0500

    Update scriptshw1.js

--- homework1/scriptshw1.js
+++ homework1/scriptshw1.js
@@ -1,44 +1,64 @@
-/* Place your JavaScript in this file */
+/* Jayden Pickin
+Oct-26-2025
+Javascript File*/
 
 const currentDate = new Date(year,month,date)
 
-const fnameinput = document.getElementById("const fname");
-const mInitialinput = document.getElementById("const mInitial");
-const lnameinput = document.getElementById("const lname");
-const dateOfBirthinput = document.getElementById("const dateOfBirth");
-const socialSecurityinput = document.getElementById("const socialSecurity");
-const addressLine1input = document.getElementById("const addressLine1");
-const addressLine2input = document.getElementById("const addressLine2");
-const cityinput = document.getElementById("const city");
-const stateinput = document.getElementById("const state");
-const zipcodeinput = document.getElementById("const zipcode");
-const emailinput = document.getElementById("const email");
-const currentSymptomsinput = document.getElementById("const currentSymptoms");
-const painLocationinput = document.getElementById("const painLocation");
-const hairColorinput = document.getElementById("const hairColor");
-const heardOfUsinput = document.getElementById("const heardOfUs");
-const drinksPerWeekinput = document.getElementById("const drinksPerWeek");
-const userIdinput = document.getElementById("const userId");
-const passwordinput = document.getElementById("const password");
+const fnameinput = document.getElementById("fname");
+const mInitialinput = document.getElementById("mInitial");
+const lnameinput = document.getElementById("name");
+const dateOfBirthinput = document.getElementById("dateOfBirth");
+const socialSecurityinput = document.getElementById("socialSecurity");
+const addressLine1input = document.getElementById("addressLine1");
+const addressLine2input = document.getElementById("addressLine2");
+const cityinput = document.getElementById("city");
+const stateinput = document.getElementById("state");
+const zipcodeinput = document.getElementById("zipcode");
+const emailinput = document.getElementById("email");
+const currentSymptomsinput = document.getElementById("currentSymptoms");
+const painLocationinput = document.getElementById("painLocation");
+const hairColorinput = document.getElementById("hairColor");
+const heardOfUsinput = document.getElementById("heardOfUs");
+const drinksPerWeekinput = document.getElementById("drinksPerWeek");
+const userIdinput = document.getElementById("userId");
+const passwordinput = document.getElementById("password");
+const password2input = document.getElementById("password2);
+
 const letterApostDashChars = /^[A-Za-z'’-]+(?: [A-Za-z'’-]+)*$/;
 const NumDashOnly = /^[123457890-]+(?: [123457890-]+)*$/;
 const lettersOnly = /^[A-Za-z]+(?: [A-Za-z]+)*$/;
+const emailFormat = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const userIDFormat = /^[A-Za-z1234567890_-]+[A-Za-z1234567890_-]+$/;
+const passwordFormat = /^(?=.*[0-9])(?=.*[!@#%^&*()-_+=\/><.,`~])(?=.*[A-Za-z])
 
+function lengthAllowed(input,maxLength,minLength){
+  if(input.length>maxLength){
+    return False
+  }
+  else if(input.length<minLength){
+    return True
+  }
+  else{
+    return False
+  }
+}
 
 function checkfname(){
   const fnameValue = fnameInput.value.trim();
-  
-  if(letterApostDashChars.test(fnameValue)){
-    document.getElementById("fnameError").innerHTML="Allowed";
-  }
-  else{
-    document.getElementById("fnameError").innerHTML="NOT ALLOWED--Letters, Apostrophes, and Dashes only";
-  }
+  if(fnameValue.length>0){
+    if(lengthAllowed(fnameValue,30,1){
+      document.getElementById("fnameError").innerHTML="NOT ALLOWED--Too Long";
+    }
+    else if(letterApostDashChars.test(fnameValue)){
+      document.getElementById("fnameError").innerHTML="Allowed";
+    }
+    else{
+      document.getElementById("fnameError").innerHTML="NOT ALLOWED--Letters, Apostrophes, and Dashes only";
+    }
 }
 
 function checkmInitial(){
   const mInitialValue = mInitialInput.value.trim();
-  
   if(lettersOnly.test(mInitialValue)){
     document.getElementById("mInitialError").innerHTML="Allowed";
   }
@@ -49,13 +69,16 @@ function checkmInitial(){
   
 function checklname(){
   const lnameValue = lnameInput.value.trim();
-  
-  if(letterApostDashNumChars.test(lnameValue)){
-    document.getElementById("lnameError").innerHTML="Allowed";
-  }
-  else{
-    document.getElementById("lnameError").innerHTML="NOT ALLOWED--Letters, Apostrophes, 2-5, and Dashes only";
-  }
+  if(lnameValue.length>0){
+    if(lengthAllowed(lnameValue,30,1){
+      document.getElementById("lnameError").innerHTML="NOT ALLOWED--Too Long";
+    }
+    else if(letterApostDashNumChars.test(lnameValue)){
+      document.getElementById("lnameError").innerHTML="Allowed";
+    }
+    else{
+      document.getElementById("lnameError").innerHTML="NOT ALLOWED--Letters, Apostrophes, 2-5, and Dashes only";
+    }
 }
   
 function checkdateOfBirth(){
@@ -86,31 +109,132 @@ function checksocialSecurity(){
 
 function checkaddressLine1(){
   //no check needed
-  if(addressLine1input.length>0){
-    document.getElementById("addressLine1Error").innerHTML="Allowed";
+  if(addressLine1Value.length>0){
+    if(lengthAllowed(addressLine1Value,30,2){
+      document.getElementById("addressLine1Error").innerHTML="NOT ALLOWED--Limited to 2-30 characters";
+    }
+    else{
+      document.getElementById("addressLine2Error").innerHTML="Allowed";
+    }
   }
 }
-function checkaddressLine2() {
+function checkaddressLine2(){
+  //no check needed
+  if(addressLine2input.length>0){
+    if(lengthAllowed(addressLine2Value,30,2){
+      document.getElementById("addressLine2Error").innerHTML="NOT ALLOWED--Limited to 2-30 characters";
+    }
+    else{
+      document.getElementById("addressLine2Error").innerHTML="Allowed";
+    }
+  }
+}
   
-function checkcity() {
+function checkcity(){
+  if(cityinput.length>0){
+    if(lengthAllowed(cityValue,30,2){
+      document.getElementById("cityError").innerHTML="NOT ALLOWED--Limited to 2-30 characters";
+    }
+    else{
+      document.getElementById("cityError").innerHTML="Allowed";
+    }
+  }
+}
   
-function checkstate() {
+function checkstate(){
+  if(stateinput=="ERRORBADNOEVILDONTDOTHISONE"){
+    document.getElementById("stateError").innerHTML="NOT ALLOWED--Please Select a Valid Option";
+  }
+  else{
+    document.getElementById("stateError").innerHTML="Allowed";
+  }
+}
   
-function checkzipcode() {
+function checkzipcode(){
+  const zipcodeValue = zipcodeInput.value.trim();
+  if(zipcodeinput.length>0){
+    if(NumDashOnly.test(socialSecurityValue)){
+      if(lengthAllowed(zipcodeValue,30,2){
+        document.getElementById("zipcodeError").innerHTML="NOT ALLOWED--Limited to 5-10 numbers";
+      }
+      else{
+        document.getElementById("zipcodeError").innerHTML="Allowed";
+      }
+    }
+    else{
+      document.getElementById("socialSecurityError").innerHTML="NOT ALLOWED--Numbers and Dashes only";
+    }
+  }
+}
   
-function checkemail() {
+function checkemail(){
+  if(emailInput.length>0){
+    if(emailFormat.test(emailInput)){
+      document.getElementById("emailError").innerHTML="Allowed";
+    }
+    else{
+      document.getElementById("emailError").innerHTML="NOT ALLOWED--Invalid Format";
+    }
+  }
+}
   
-function checkcurrentSymptoms() {
+function checkcurrentSymptoms(){
+  if(currentSymptomsInput.inclues('"'){
+    document.getElementById("emailError").innerHTML='NOT ALLOWED--" Is not allowed';
+  }
+}
   
-function checkpainLocation() {
+function checkpainLocation(){
+  //not needed right now
+}
   
-function checkhairColor() {
+function checkhairColor(){
+  //not needed right now
+}
   
-function checkheardOfUs() {
+function checkheardOfUs(){
+  //not needed right now
+}
   
-function checkdrinksPerWeek() {
+function checkdrinksPerWeek(){
+  //not needed right now
+}
   
-function checkuserId() {
+function checkuserId(){
+  if(userIDInput.length>0){
+    if(userIDFormat.test(userIDInput)){
+      if(lengthAllowed(userIDInput,5,30){
+         document.getElementById("userIDError").innerHTML="Allowed";
+      }
+      else{
+        document.getElementByID("userIDError").innerHTML="NOT ALLOWED--Invalid Length 5-30 characters")
+      }
+    }
+    else{
+      document.getElementById("userIDError").innerHTML="NOT ALLOWED--Must be letter, number or dashes";
+    }
+  }
+}
   
-function checkpassword() {
-  
+function checkpassword(){
+  if(passwordInput.length>0){
+    if(passwordFormat.test(passwordInput)){
+      if(lengthAllowed(passwordInput,8,30){
+         document.getElementById("passwordFormatError").innerHTML="Allowed";
+      }
+      else{
+        document.getElementByID("passwordFormatError").innerHTML="NOT ALLOWED--Invalid Length 8-30 characters")
+      }
+    }
+    else{
+      document.getElementById("passwordFormatError").innerHTML="NOT ALLOWED--Must be letter, number or dashes";
+    }
+  }
+}
+
+function checkpasswordmatch(){
+  if(passwordInput==password2Input){
+    document.getElementById("password2Error").innerHTML="Passwords Don't match";
+  }
+}
+
